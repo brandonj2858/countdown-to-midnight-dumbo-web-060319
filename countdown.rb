6f63f9e86b398f2919a2df4864ef9970a0a74aaf
Brandon James <brandonj2858@gmail.com>
2019-05-17 01:29:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
-#write your code here
 
-def countdown
+
+def countdown(int)
+  int = 10
+while int > 0
+puts "#{int} SECOND(S)!"
+int -= 1
+end
+return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  int = 10
+  while int > 0 
+  puts "#{int} SECOND(S)"
+  sleep 1
+end 
+return "HAPPY NEW YEAR!"
 end
